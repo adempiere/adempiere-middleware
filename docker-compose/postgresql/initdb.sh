7588c654914b17a5efc3e6b5186644fd4c129387
yamelsenih <ysenih@erpya.com>
2023-03-16 09:44:58 -0400

    Add compress database

--- docker-compose/postgresql/initdb.sh
+++ docker-compose/postgresql/initdb.sh
@@ -5,5 +5,5 @@ then
     createuser -U postgres adempiere -dlrs
     psql -U postgres -tAc "alter user adempiere password 'adempiere';"
     createdb -U adempiere adempiere
-    psql -U adempiere -d adempiere < Adempiere/data/Adempiere_pg.dmp
+    psql -U adempiere -d adempiere < Adempiere_pg.dmp
 fi
